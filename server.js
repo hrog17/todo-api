3cc510c09c126e54bc9d71541134a2dbda074e09
Henry Rogando <henry.rogando@pb.com>
2016-02-03 09:59:05 -0500

    add POST body validation and initial cleansing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3001;
@@ -22,28 +23,28 @@ app.get('/todos', function (req, res){
 app.get('/todos/:id', function (req,res) {
   var todoId = parseInt(req.params.id,10);
   // iterate over todos array to find the match
-  var todoObj;
+  var matchTodo = _.findWhere(todos, {id: todoId});
 
-  todos.forEach(function(todo) {
-    if (todo.id === todoId) {
-       todoObj = todo;
-    }
-  });
-
-  if ( typeof todoObj === 'undefined') {
+  if ( typeof matchTodo === 'undefined') {
     //res.json(todoObj);
     res.status(404).send();
   }
   else {
     console.log ('hit');
     //res.status(404).send();
-    res.json(todoObj);
+    res.json(matchTodo);
   }
 });
 
 // POST /todos
 app.post('/todos',function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+      return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
 
     body.id = todoNextId++;
     //console.log('description: ' + body.description);
