211461230c83bcf3a8c9449c3ad0ee7a29209477
Henry Rogando <henry.rogando@pb.com>
2016-02-03 09:27:10 -0500

    add POST /todos

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3001;
 
-var todos = [{
-  id: 1,
-  description: 'Take kids to kungfu.',
-  completed: false
-},{
-  id: 2,
-  description: 'Go to Market.',
-  completed: false
-},{
-  id: 3,
-  description: 'Have some snacks.',
-  completed: true
-}];
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 
 app.get('/', function (req, res){
   res.send('Todo API Root');
@@ -26,6 +18,7 @@ app.get('/todos', function (req, res){
   res.json(todos);
 });
 
+// GET /todos/id
 app.get('/todos/:id', function (req,res) {
   var todoId = parseInt(req.params.id,10);
   // iterate over todos array to find the match
@@ -46,6 +39,17 @@ app.get('/todos/:id', function (req,res) {
     //res.status(404).send();
     res.json(todoObj);
   }
+});
+
+// POST /todos
+app.post('/todos',function (req, res) {
+    var body = req.body;
+
+    body.id = todoNextId++;
+    //console.log('description: ' + body.description);
+    todos.push(body);
+
+    res.status(201).json (body);
 });
 
 
