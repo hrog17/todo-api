6d17dfe40c9366ca84997a443191c7758ac872fd
Henry Rogando <henry.rogando@pb.com>
2016-02-03 16:31:44 -0500

    add ?q qyery parameter for /todos

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res){
   res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=false&q=work
 app.get('/todos', function (req, res){
   var queryParams = req.query;
   var filteredTodos = todos;
@@ -23,9 +23,15 @@ app.get('/todos', function (req, res){
   // call .where(filteredTodos,?)
 
   if ( queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-    filteredTodos = _.where(todos,{completed: true});
+    filteredTodos = _.where(filteredTodos,{completed: true});
   } else if ( queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-    filteredTodos = _.where(todos,{completed: false});
+    filteredTodos = _.where(filteredTodos,{completed: false});
+  }
+
+  if ( queryParams.hasOwnProperty('q') && queryParams.q.trim().length > 0) {
+    filteredTodos = _.filter(filteredTodos,function(todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    });
   }
 
   res.json(filteredTodos);
---
+++ variable-playground.js
@@ -0,0 +1,31 @@
+// var person = {
+//   name: 'Andrew',
+//   age: 21
+// };
+//
+// function updatePerson (obj) {
+//   // obj = {
+//   //   name: 'Andrew',
+//   //   age: 24
+//   // };
+//   obj.age = 24;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+var grades = [10, 25];
+
+//Array Example
+function addGrades(gradesArr) {
+  gradesArr.push(12);
+  debugger;
+}
+
+// function updateArray(array) {
+//
+// }
+
+addGrades(grades);
+console.log (grades)
+//updateArray(grades);
