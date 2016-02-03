a28f2985d09e48780e3f2dfe7e52fece450dd138
Henry Rogando <henry.rogando@pb.com>
2016-02-03 10:19:41 -0500

    add DELETE /todos

--- server.js
+++ server.js
@@ -53,6 +53,18 @@ app.post('/todos',function (req, res) {
     res.status(201).json (body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req,res) {
+  var todoId = parseInt(req.params.id,10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo) {
+    res.status(404).json({"error":"No todo found with that id"});
+  } else {
+    todos = _.without(todos, matchedTodo);
+    res.json (matchedTodo);
+  }
+});
 
 app.listen(PORT, function() {
   console.log ('application is listening on port ' + PORT);
