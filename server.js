02c394f8260bd0420bc853cadf722018603c5ed9
Henry Rogando <henry.rogando@pb.com>
2016-02-08 15:18:49 -0500

    Add password hash and salt

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ app.post('/users', function(req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
     db.user.create(body).then(function(user) {
-        return res.json(user.toJSON());
+        return res.json(user.toPublicJSON());
     }, function(e) {
         return res.status(400).json(e);
     });
