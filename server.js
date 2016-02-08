cf8449a48085e014eaf891d576d2f0d8b97c6762
Henry Rogando <henry.rogando@pb.com>
2016-02-08 10:08:52 -0500

    Convert PUT /todos/:id to sequelize

--- server.js
+++ server.js
@@ -160,34 +160,29 @@ app.delete('/todos/:id', function(req, res) {
 app.put('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
     var body = _.pick(req.body, 'description', 'completed');
-    var matchedTodo = _.findWhere(todos, {
-        id: todoId
+    var attributes = {};
+
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
+    }
+
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
+    }
+
+    db.todo.findById(todoId).then(function(todo) {
+        if (todo) {
+            return todo.update(attributes);
+        } else {
+            return res.status(404).send();
+        }
+    }, function() {
+        return res.status(500).send();
+    }).then(function(todo) {
+        return res.json(todo.toJSON());
+    }, function(e) {
+        return res.status(400).json(e.toJSON());
     });
-
-    var validAttributes = {};
-
-    if (!matchedTodo) {
-        return res.status(404).json({
-            "error": "No todo found with that id"
-        });
-    }
-
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        return res.status(400).send();
-    }
-
-    if (body.hasOwnProperty('description') && _.isString(body.description) &&
-        body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return res.status(400).send();
-    }
-    // HERE  (passed by reference)
-    _.extend(matchedTodo, validAttributes);
-
-    res.json(matchedTodo);
 });
 
 db.sequelize.sync().then(function() {
