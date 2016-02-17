47e4970cff50b024be5818b7ddac296cb4da007c
Henry Rogando <henry.rogando@pb.com>
2016-02-17 09:11:21 -0500

    Add force:true for heroku

--- server.js
+++ server.js
@@ -217,7 +217,9 @@ app.post('/users/login', function(req, res) {
         });
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({
+    force: true
+}).then(function() {
     app.listen(PORT, function() {
         console.log('application is listening on port ' +
             PORT);
