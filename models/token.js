b5b0a6a339e2c89f3f36a535553a6049c26a33fe
Henry Rogando <henry.rogando@pb.com>
2016-02-17 09:53:55 -0500

    Add DELETE /users/login operation

---
+++ models/token.js
@@ -0,0 +1,21 @@
+// model to store token (hash value of token)
+var cryptojs = require('crypto-js');
+
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('token', {
+        token: {
+            type: DataTypes.VIRTUAL,
+            allowNull: false,
+            validate: {
+                len: [1]
+            },
+            set: function(value) {
+                var hash = cryptojs.MD5(value).toString();
+                this.setDataValue('token', value);
+                this.setDataValue('tokenHash',
+                    hash);
+            }
+        },
+        tokenHash: DataTypes.STRING
+    });
+};
